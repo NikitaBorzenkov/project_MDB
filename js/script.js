e381999094b99977f70fe40688b400943a99e7b5
Nikita Borzenkov <nikita.borzoi@gmail.com>
2022-04-11 23:44:49 +0300

    Начальная модификация файла скрипта

--- js/script.js
+++ js/script.js
@@ -24,3 +24,28 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      movieList = document.querySelector('.promo__interactive-list'),
+      genre = poster.querySelector('.promo__genre');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = `url(img/bg.jpg)`;
+
+movieList.innerHTML = " ";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
+
